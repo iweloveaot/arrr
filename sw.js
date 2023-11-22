262d8a4a16ac37a11c64cbc67d4eea614d1b6b7e
vinny <viknikgon@yandex.ru>
2023-11-22 14:43:41 +0300

    делаем pwa

--- sw.js
+++ sw.js
@@ -1,12 +1,2 @@
-importScripts('sw-toolbox.js');
-
-toolbox.precache([
-  'companion.js',
-  'index.html',
-  'main.js',
-  'styles.css',
-]);
-toolbox.router.default = toolbox.networkFirst;
-toolbox.options.networkTimeoutSeconds = 5;
-
-toolbox.router.get('icons/*', toolbox.fastest);
+‘use strict’;
+importScripts(‘sw-toolbox.js’); toolbox.precache([“index.html”,”style/style.css”]); toolbox.router.get(‘/images/*’, toolbox.cacheFirst); toolbox.router.get(‘/*’, toolbox.networkFirst, { networkTimeoutSeconds: 5});
