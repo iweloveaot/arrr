96c7ecf42c1a77bd427c0230778d7c96d9c531bc
Samina <Aydar.Sattarov@russianpost.ru>
2023-12-05 20:47:45 +0300

    блютуз починился

--- main.js
+++ main.js
@@ -228,7 +228,7 @@ function send(data) {
     for (let i = 1; i < chunks.length; i++) {
       setTimeout(() => {
         writeToCharacteristic(characteristicCache, chunks[i]);
-      }, i * 100);
+      }, i * 200);
     }
   }
   else {
